e58ef6c3dba68c8d63cd70d018751bf2adcd2883
adamchaboryk <32206034+adamchaboryk@users.noreply.github.com>
2021-04-19 12:42:28 -0400

    Update bm-sa11y-v2.js

--- bookmarklet/bm-sa11y-v2.js
+++ bookmarklet/bm-sa11y-v2.js
@@ -8,19 +8,29 @@ bodyheader.appendChild(sa11ycss);
 
 /* Append jQuery, Poppers.js, Tippy.js, and Sa11y in specific order. */
 document.body.appendChild(document.createElement("script")).src = "https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js";
+
 var popperJS = document.createElement("script");
 popperJS.src = "https://unpkg.com/@popperjs/core@2";
+
 var tippyJS = document.createElement("script");
 tippyJS.src = "https://unpkg.com/tippy.js@6";
+
 var sa11yJS1 = document.createElement("script");
 sa11yJS1.src = "https://cdn.jsdelivr.net/gh/ryersondmp/sa11y@main-v1.1/sa11y-constant.js";
+
 var sa11yJS2 = document.createElement("script");
 sa11yJS2.src = "https://cdn.jsdelivr.net/gh/ryersondmp/sa11y@main-v1.1/sa11y-v2.js";
+
 document.body.appendChild(popperJS);
+
 popperJS.onload = popperJS.onreadystatechange = function() {
   document.body.appendChild(tippyJS);
+
   tippyJS.onload = tippyJS.onreadystatechange = function() {
     document.body.appendChild(sa11yJS1);
-    document.body.appendChild(sa11yJS2);
+      
+      sa11yJS1.onload = sa11yJS1.onreadystatechange = function() {
+      document.body.appendChild(sa11yJS2);
+    };
   };
 };
