18ae85e447436836a3e6c47513fbc90248056578
adamchaboryk <32206034+adamchaboryk@users.noreply.github.com>
2021-04-27 20:06:41 -0400

    Update sa11y-v2.js

--- sa11y-v2.js
+++ sa11y-v2.js
@@ -219,27 +219,6 @@ class Sa11y {
                 this.reset();
             });
 
-            //https://stackoverflow.com/questions/6390341/how-to-detect-if-url-has-changed-after-hash-in-javascript
-            history.pushState = ( f => function pushState(){
-                var ret = f.apply(this, arguments);
-                window.dispatchEvent(new Event('pushstate'));
-                window.dispatchEvent(new Event('locationchange'));
-                return ret;
-            })(history.pushState);
-            
-            history.replaceState = ( f => function replaceState(){
-                var ret = f.apply(this, arguments);
-                window.dispatchEvent(new Event('replacestate'));
-                window.dispatchEvent(new Event('locationchange'));
-                return ret;
-            })(history.replaceState);
-            
-            window.addEventListener('popstate',()=>{
-                window.dispatchEvent(new Event('locationchange'))
-                this.checkAll();
-                this.reset();
-            });
-
             //Escape key to shutdown.
             $(document).keyup((escape) => {
                 if (
