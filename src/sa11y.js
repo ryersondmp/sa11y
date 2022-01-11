53ddb4c7d81b44f8a88f9d4d72807c62acf885a8
Adam Chaboryk <adam.chaboryk@gmail.com>
2022-01-11 00:14:04 -0500

    bookmarklet testing w/ setTimeout

--- src/sa11y.js
+++ src/sa11y.js
@@ -222,38 +222,23 @@ class Sa11y {
 
 			console.log("Sa11y interface loaded.");
 
-			//Swapp load event listener with onreadystatechange
-			document.onreadystatechange = () => {
-				if (document.readyState == "complete") {
+			//Wrap a setTimeout of 1ms for bookmarklet because of load event.
+			setTimeout(() => {
+				window.addEventListener('load', () => {
+					console.log("Page is fully loaded. Loading Sa11y now.");
 					this.globals();
 					this.mainToggle();
 					this.utilities();
 					this.skipToIssueTooltip();
-					
+	
 					document.getElementById("sa11y-toggle").disabled = false;
 					if (localStorage.getItem("sa11y-remember-panel") === "Closed" || !localStorage.getItem("sa11y-remember-panel")) {
 						this.panelActive = true;
 						this.checkAll();
-						console.log("Sa11y enabled.");
+						console.log("Sa11y enabled.")
 					}
-				}
-			}
-
-			/* window.addEventListener('load', () => {
-				console.log("Page is fully loaded. Loading Sa11y now.");
-				this.globals();
-				this.mainToggle();
-				this.utilities();
-				this.skipToIssueTooltip();
-				
-				document.getElementById("sa11y-toggle").disabled = false;
-				if (localStorage.getItem("sa11y-remember-panel") === "Closed" || !localStorage.getItem("sa11y-remember-panel")) {
-					this.panelActive = true;
-					this.checkAll();
-					console.log("Sa11y enabled.")
-				}
-			}); */
-
+				});
+			}, 1);
 		};
 
 		this.globals = () => {
