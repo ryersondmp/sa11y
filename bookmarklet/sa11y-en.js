b1e8221e731e4ad12195d9a2b59d0fea10f9ab56
Adam Chaboryk <adam.chaboryk@gmail.com>
2022-03-25 15:59:20 -0400

    Quick patch for Firefox.

--- bookmarklet/sa11y-en.js
+++ bookmarklet/sa11y-en.js
@@ -15,7 +15,7 @@
 /* Append sa11y.css */
 const sa11ycss = document.createElement("link");
 sa11ycss.setAttribute("rel", "stylesheet");
-sa11ycss.setAttribute("href", "https://cdn.jsdelivr.net/gh/ryersondmp/sa11y@2.1.2/src/sa11y.min.css");
+sa11ycss.setAttribute("href", "https://cdn.jsdelivr.net/gh/ryersondmp/sa11y@2.1.3/src/sa11y.min.css");
 sa11ycss.setAttribute("type", "text/css");
 
 const bodyheader = document.getElementsByTagName("head")[0];
@@ -23,7 +23,7 @@ bodyheader.appendChild(sa11ycss);
 
 /* Queue Poppers.js, Tippy.js, and Sa11y. Thanks to JSDeliver for this cool combine feature! */
 const combine = document.createElement("script");
-combine.src = "https://cdn.jsdelivr.net/combine/npm/@popperjs/core@2/dist/umd/popper.min.js,npm/tippy.js@6/dist/tippy.umd.min.js,gh/ryersondmp/sa11y@2.1.2/src/sa11y-english.min.js,gh/ryersondmp/sa11y@2.1.2/src/sa11y.min.js";
+combine.src = "https://cdn.jsdelivr.net/combine/npm/@popperjs/core@2/dist/umd/popper.min.js,npm/tippy.js@6/dist/tippy.umd.min.js,gh/ryersondmp/sa11y@2.1.3/src/sa11y-english.min.js,gh/ryersondmp/sa11y@2.1.3/src/sa11y.min.js";
 
 document.body.appendChild(combine);
 combine.onload = combine.onreadystatechange = function() {
