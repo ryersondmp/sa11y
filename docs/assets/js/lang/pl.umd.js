b43dadaa7a2f914e322a8f236ab62bbd4768b693
Adam Chaboryk <adam.chaboryk@gmail.com>
2025-04-21 15:08:14 -0400

    More concise theming with `color-scheme: light dark`

--- docs/assets/js/lang/pl.umd.js
+++ docs/assets/js/lang/pl.umd.js
@@ -1,7 +1,7 @@
 
 /*!
   * Sa11y, the accessibility quality assurance assistant.
-  * @version 4.1.8
+  * @version 4.1.9
   * @author Adam Chaboryk
   * @license GPL-2.0-or-later
   * @copyright © 2020 - 2025 Toronto Metropolitan University.
